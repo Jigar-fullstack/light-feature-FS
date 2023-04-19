7d8c2cac89f9e7e932189384312ffb644100700d
Jigar Desai <jigar.fullstack@gmail.com>
2023-04-19 11:15:35 +0530

    Initial commit of project

---
+++ src/main/java/com/example/light/feature/supervisiors/SupervisiorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.light.feature.supervisiors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisiorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervisiorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/light/feature/supervisiors/SupervisiorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.light.feature.supervisiors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisiorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
